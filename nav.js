8e670e09f9a514bf1a871f5035f475e3b6cdfa5d
Marcelo <57197501+marcelo-site@users.noreply.github.com>
2023-05-16 20:49:31 -0300

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,16 +1,17 @@
+(function () {
+    const li = document.querySelectorAll('nav ul li')
+    let largura = window.screen.width;
+    const toggle = document.querySelector('#toggle')
+    if (largura < 666) {
+        li.forEach(el => el.addEventListener('click', () => toggle.click()))
+    }
+}) ()
 const toggle = document.querySelector('#toggle')
 const toggleOn = document.querySelector('#toogle-on')
 const toggleOff = document.querySelector('#toggle-off')
 const navUl = document.querySelector('nav ul')
 const background1 = document.querySelector('#background1')
-function widthLi () {
-    const li = document.querySelectorAll('nav ul li')
-    let largura = window.screen.width;
-    if (largura < 666) {
-        li.forEach(el => el.addEventListener('click', () => toggle.click()))
-    }
-}
-widthLi()
+
 toggle.addEventListener('click', () => {
     toggleOff.classList.toggle('none')
     toggleOn.classList.toggle('none')
